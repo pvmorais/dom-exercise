f5d26d7a5ef7b98661635c44745587b453345b36
Vinicius de Morais <pvmoraiss@gmail.com>
2023-02-07 11:53:37 -0600

    exercise DOM events in js more methods

--- dom-exercise-events/index.js
+++ dom-exercise-events/index.js
@@ -7,3 +7,18 @@ const btn3 = document.querySelector('#btn3');
 btn3.addEventListener('click', () => {
   alert("Hello World");
 });
+
+//METHOD 1 - named functions
+function alertFunction() {
+    alert("YAY! YOU DID IT!");
+  }
+
+// METHOD 2
+btn.onclick = alertFunction;
+
+// METHOD 3
+btn3.addEventListener('click', alertFunction);
+
+btn3.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+});
